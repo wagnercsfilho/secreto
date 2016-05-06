7f5d68b74ea2bebd093f489ed1714f1f87c5947e
wagnercsfilho <wagnercsfilho@hotmail.com>
2016-05-06 08:31:45 +0000

    add statusbar m

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve('./www/dist/'),
-        filename: 'js/bundle.js',
-        publicPath: '/'
+        filename: 'js/bundle.js'
     },
     module: {
         loaders: [{
@@ -21,7 +20,7 @@ module.exports = {
         { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader") },
         { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader!autoprefixer-loader") },
     //    { test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=100000" },
-        { test: /\.(png|jpg|gif)$/, loader: "file-loader?name=dist/img/img-[hash:6].[ext]" }]
+        { test: /\.(png|jpg|gif)$/, loader: "file-loader?name=../img/img-[hash:6].[ext]" }]
     },
     // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
     plugins: [
